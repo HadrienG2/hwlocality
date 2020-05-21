d5cf7c1fac31dec8a5858b353c97a82314ee1b00
Joe Hirschfeld <joe@ibj.io>
2020-05-21 01:35:15 -0700

    Update Cargo.toml and build.rs with versions

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 extern crate pkg_config;
 
 fn main() {
-    let probed = pkg_config::Config::new().atleast_version("1.11.0").probe("hwloc");
+    let probed = pkg_config::Config::new().atleast_version("2.0.0").probe("hwloc");
     if probed.is_ok() {
         return;
     }
