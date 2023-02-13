d75a5f6bb34d0db614b552fc5e0c00c1f7967d54
Hadrien Grasland <grasland@lal.in2p3.fr>
2023-02-13 16:02:56 +0100

    Flesh out editor design

--- src/editor.rs
+++ src/editor.rs
@@ -4,18 +4,28 @@
 //! avoid the thread-unsafety and keep `impl Sync for Topology` by forcing eager
 //! evaluation of the lazy caches at the end of editing, but as a result of this
 //! design, topology modifications must be carried out through a proxy object
-//! that does not permit unevaluated caches to escape.
+//! that does not permit shared references to unevaluated caches to escape.
 
-use std::panic::UnwindSafe;
-
-use crate::Topology;
+use crate::{RawTopology, Topology};
 
 /// Proxy for modifying a `Topology`
 pub struct TopologyEditor<'topology>(&'topology mut Topology);
 
 impl<'topology> TopologyEditor<'topology> {
+    // === Modifying a loaded Topology: https://hwloc.readthedocs.io/en/v2.9/group__hwlocality__tinker.html ===
+
+    // TODO: insert_xyz_objects will require weird tricks because by design they
+    //       violate Rust's shared XOR mutability rules.
+
+    // === General-purpose internal utilities ===
+
     /// Wrap an `&mut Topology` into a topology editor
     pub(crate) fn new(topology: &'topology mut Topology) -> Self {
         Self(topology)
+    }
+
+    /// Returns the contained hwloc topology pointer for interaction with hwloc
+    fn as_mut_ptr(&mut self) -> *mut RawTopology {
+        self.0.as_mut_ptr()
     }
 }
