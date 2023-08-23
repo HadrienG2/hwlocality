473238fd0f4bd99d8bce0ed6f3a2a973d42026ff
Hadrien G <knights_of_ni@gmx.com>
2023-08-23 08:50:54 +0200

    Clean things up, and hopefully fix CI along the way

--- build.rs
+++ build.rs
@@ -2,61 +2,20 @@
 use std::{
     env,
     path::{Path, PathBuf},
-    process::Command
+    process::Command,
 };
 
-#[cfg(feature = "bundled")]
-fn fetch_hwloc(path: &Path, version: &str) {
-    if !path.join("hwloc").join("Makefile.am").exists() {
-        let r = Command::new("git")
-            .args(&[
-                "clone",
-                "https://github.com/open-mpi/hwloc",
-                "--depth",
-                "1",
-                "--branch",
-                version,
-            ])
-            .current_dir(path.clone())
-            .output();
-        let m = r.expect("Failed to run git clone");
-        assert!(
-            m.status.success(),
-            "git clone for hwloc returned failure code {}\n{:?}",
-            m.status,
-            m
-        );
-    }
-}
-
-#[cfg(feature = "bundled")]
-fn compile_hwloc_autotools(p: PathBuf) -> PathBuf {
-    let mut config = autotools::Config::new(p);
-    config.reconf("-ivf").make_target("install").build()
-}
-
-#[cfg(feature = "bundled")]
-fn compile_hwloc_cmake(build_path: &Path) -> PathBuf {
-    let mut cfg = cmake::Config::new(build_path);
-    if let Ok(profile) = env::var("HWLOC_BUILD_PROFILE") {
-        cfg.profile(&profile);
-    } else {
-        cfg.profile("release");
-    }
-
-    // Allow specifying custom toolchain specifically for hwloc.
-    if let Ok(toolchain) = env::var("HWLOC_TOOLCHAIN") {
-        cfg.define("CMAKE_TOOLCHAIN_FILE", &toolchain);
-    }
-    cfg.build()
-}
-
+// Use pkg-config to configure the build for a certain hwloc release
 fn use_pkgconfig(required_version: &str, first_unsupported_version: &str) -> pkg_config::Library {
+    // Run pkg-config
     let lib = pkg_config::Config::new()
         .range_version(required_version..first_unsupported_version)
         .statik(true)
         .probe("hwloc")
         .expect("Could not find a suitable version of hwloc");
+
+    // As it turns-out, pkg-config does not correctly set up the RPATHs for the
+    // transitive dependencies of hwloc itself in static builds. Fix that.
     if cfg!(target_family = "unix") {
         for link_path in &lib.link_paths {
             println!(
@@ -67,10 +26,78 @@ fn use_pkgconfig(required_version: &str, first_unsupported_version: &str) -> pkg
             );
         }
     }
+
+    // Forward pkg-config output for futher consumption
     lib
 }
 
+// Fetch hwloc from a git release branch, return repo path
+#[cfg(feature = "bundled")]
+fn fetch_hwloc(parent_path: impl AsRef<Path>, version: &str) -> PathBuf {
+    // Determine location of the git repo and its parent directory
+    let parent_path = parent_path.as_ref();
+    let repo_path = parent_path.join("hwloc");
+
+    // Clone the repo if this is the first time, update it with pull otherwise
+    let output = if !repo_path.join("Makefile.am").exists() {
+        Command::new("git")
+            .args([
+                "clone",
+                "https://github.com/open-mpi/hwloc",
+                "--depth",
+                "1",
+                "--branch",
+                version,
+            ])
+            .current_dir(parent_path)
+            .output()
+            .expect("git clone for hwloc failed")
+    } else {
+        Command::new("git")
+            .args(["pull", "--ff-only", "origin", "v2.x"])
+            .current_dir(&repo_path)
+            .output()
+            .expect("git pull for hwloc failed")
+    };
+
+    // Make sure the command returned a successful status
+    let status = output.status;
+    assert!(
+        status.success(),
+        "git clone/pull for hwloc returned failure status {status}:\n{output:?}"
+    );
+
+    // Propagate repo path
+    repo_path
+}
+
+// Compile hwloc using autotools, return local installation path
+#[cfg(all(feature = "bundled", not(windows)))]
+fn compile_hwloc_autotools(p: PathBuf) -> PathBuf {
+    let mut config = autotools::Config::new(p);
+    config.fast_build(true).reconf("-ivf").build()
+}
+
+// Compile hwloc using cmake, return local installation path
+#[cfg(all(feature = "bundled", windows))]
+fn compile_hwloc_cmake(build_path: &Path) -> PathBuf {
+    let mut config = cmake::Config::new(build_path);
+
+    // Allow specifying the CMake build profile
+    if let Ok(profile) = env::var("HWLOC_BUILD_PROFILE") {
+        config.profile(&profile);
+    }
+
+    // Allow specifying the build toolchain
+    if let Ok(toolchain) = env::var("HWLOC_TOOLCHAIN") {
+        config.define("CMAKE_TOOLCHAIN_FILE", &toolchain);
+    }
+
+    config.always_configure(false).build()
+}
+
 fn main() {
+    // Determine the minimal supported hwloc version with current featurees
     let required_version = if cfg!(feature = "hwloc-2_8_0") {
         "2.8.0"
     } else if cfg!(feature = "hwloc-2_5_0") {
@@ -88,9 +115,11 @@ fn main() {
     } else {
         "2.0.0"
     };
+
+    // If asked to build hwloc ourselves...
     #[cfg(feature = "bundled")]
     {
-        let git_dir = env::var("OUT_DIR").expect("No output directory given");
+        // Determine which version to fetch and where to fetch it
         let (source_version, first_unsupported_version) = match required_version
             .split('.')
             .next()
@@ -99,43 +128,45 @@ fn main() {
             "2" => ("v2.x", "3.0.0"),
             other => panic!("Please add support for bundling hwloc v{other}.x"),
         };
-        let mut hwloc_source_path = PathBuf::from(git_dir);
-        fetch_hwloc(hwloc_source_path.as_path(), source_version);
-        hwloc_source_path.push("hwloc");
-        let cmake = cfg!(target_os = "windows")
-            && hwloc_source_path
-                .join("contrib")
-                .join("windows-cmake")
-                .join("CMakeLists.txt")
-                .exists();
-        let hwloc_compiled_path: PathBuf = if cmake {
-            hwloc_source_path.push("contrib");
-            hwloc_source_path.push("windows-cmake");
-            compile_hwloc_cmake(hwloc_source_path.as_path())
-        } else {
-            compile_hwloc_autotools(hwloc_source_path)
-        };
+        let out_path = env::var("OUT_DIR").expect("No output directory given");
 
+        // Fetch latest supported hwloc from git
+        let source_path = fetch_hwloc(out_path, source_version);
+
+        // On Windows, we build using CMake because the autotools build
+        // procedure does not work with MSVC, which is often needed on this OS
         #[cfg(target_os = "windows")]
         {
+            // Locate CMake support files, make sure they are present
+            // (should be the case on any hwloc release since 2.8)
+            let cmake_path = source_path.join("contrib").join("windows-cmake");
+            assert!(
+                cmake_path.join("CMakeLists.txt").exists(),
+                "Need hwloc's CMake support to build on Windows (with MSVC)"
+            );
+
+            // Build hwloc, configure our own build to use it
+            let install_path = compile_hwloc_cmake(cmake_path);
             println!("cargo:rustc-link-lib=static=hwloc");
             println!(
                 "cargo:rustc-link-search={}",
-                hwloc_compiled_path.join("lib").display()
+                install_path.join("lib").display()
             );
         }
+
+        // On other OSes, we build using autotools and configure using pkg-config
         #[cfg(not(target_os = "windows"))]
         {
+            let install_path = compile_hwloc_autotools(source_path);
             env::set_var(
                 "PKG_CONFIG_PATH",
-                format!(
-                    "{}",
-                    hwloc_compiled_path.join("lib").join("pkgconfig").display()
-                ),
+                format!("{}", install_path.join("lib").join("pkgconfig").display()),
             );
             use_pkgconfig(required_version, first_unsupported_version);
         }
     }
+
+    // If asked to use system hwloc, we configure it using pkg-config
     #[cfg(not(feature = "bundled"))]
     {
         let first_unsupported_version = match required_version
