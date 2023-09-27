607c0ce774e1c14f507d3308ba2eb8154875e752
Hadrien Grasland <grasland@lal.in2p3.fr>
2023-09-27 19:05:06 +0200

    Try another docs.rs detection method

--- hwlocality-sys/build.rs
+++ hwlocality-sys/build.rs
@@ -5,7 +5,7 @@ use std::{
     process::Command,
 };
 
-// Use pkg-config to configure the build for a certain hwloc release
+/// Use pkg-config to configure the build for a certain hwloc release
 #[cfg(not(all(feature = "bundled", windows)))]
 fn use_pkgconfig(required_version: &str) -> pkg_config::Library {
     // Determine the first unsupported version
@@ -42,7 +42,7 @@ fn use_pkgconfig(required_version: &str) -> pkg_config::Library {
     lib
 }
 
-// Fetch hwloc from a git release branch, return repo path
+/// Fetch hwloc from a git release branch, return repo path
 #[cfg(feature = "bundled")]
 fn fetch_hwloc(parent_path: impl AsRef<Path>, version: &str) -> PathBuf {
     // Determine location of the git repo and its parent directory
@@ -82,14 +82,14 @@ fn fetch_hwloc(parent_path: impl AsRef<Path>, version: &str) -> PathBuf {
     repo_path
 }
 
-// Compile hwloc using autotools, return local installation path
+/// Compile hwloc using autotools, return local installation path
 #[cfg(all(feature = "bundled", not(windows)))]
 fn compile_hwloc_autotools(source_path: impl AsRef<Path>) -> PathBuf {
     let mut config = autotools::Config::new(source_path);
     config.fast_build(true).reconf("-ivf").build()
 }
 
-// Compile hwloc using cmake, return local installation path
+/// Compile hwloc using cmake, return local installation path
 #[cfg(all(feature = "bundled", windows))]
 fn compile_hwloc_cmake(cmake_path: impl AsRef<Path>) -> PathBuf {
     let mut config = cmake::Config::new(cmake_path);
@@ -107,7 +107,7 @@ fn compile_hwloc_cmake(cmake_path: impl AsRef<Path>) -> PathBuf {
     config.always_configure(false).build()
 }
 
-#[cfg(not(docsrs))]
+/// Configure the hwloc dependency
 fn setup_hwloc() {
     // Determine the minimal supported hwloc version with current featurees
     let required_version = if cfg!(feature = "hwloc-2_8_0") {
@@ -206,6 +206,8 @@ fn setup_hwloc() {
 }
 
 fn main() {
-    #[cfg(not(docsrs))]
-    setup_hwloc();
+    // We don't need hwloc on docs.rs since it only builds the docs
+    if !std::env::var("DOCS_RS").is_ok() {
+        setup_hwloc();
+    }
 }
