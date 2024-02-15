b402c86ed58e58c9e91d9ad6f3865c050f5518a6
Hadrien Grasland <hadrien.grasland@ijclab.in2p3.fr>
2024-02-15 16:14:53 +0100

    Clean up cache stats docs and add tests

--- src/cpu/cache.rs
+++ src/cpu/cache.rs
@@ -20,9 +20,12 @@ use similar_asserts::assert_eq;
 impl Topology {
     /// Compute CPU cache statistics, if cache sizes are known
     ///
+    /// Returns None if cache size information is unavailable for at least some
+    /// of the CPU caches on the system.
+    ///
     /// These statistics can be used to perform simple cache locality
     /// optimizations when your performance requirements do not call for full
-    /// locality-aware scheduling with manual task and memory pinning.
+    /// locality-aware scheduling with careful task and memory pinning.
     ///
     /// This functionality is an hwlocality-specific extension to the hwloc API.
     ///
@@ -49,34 +52,13 @@ impl Topology {
     ///     "Total data cache sizes: {:?}",
     ///     stats.total_data_cache_sizes()
     /// );
-    ///
-    /// assert_eq!(stats.smallest_data_cache_sizes().len(),
-    ///            stats.smallest_data_cache_sizes_per_thread().len());
-    /// assert_eq!(stats.smallest_data_cache_sizes().len(),
-    ///            stats.total_data_cache_sizes().len());
-    /// for ((smallest, smallest_per_thread), total) in
-    ///     stats.smallest_data_cache_sizes().iter()
-    ///         .zip(stats.smallest_data_cache_sizes_per_thread())
-    ///         .zip(stats.total_data_cache_sizes())
-    /// {
-    ///     assert!(smallest_per_thread <= smallest);
-    ///     assert!(smallest <= total);
-    /// }
+    /// #
     /// # Ok::<(), eyre::Report>(())
     /// ```
     pub fn cpu_cache_stats(&self) -> Option<CpuCacheStats> {
         CpuCacheStats::new(self)
     }
 }
-
-/// Data (or unified) caches levels supported by hwloc
-const DATA_CACHE_LEVELS: &[ObjectType] = &[
-    ObjectType::L1Cache,
-    ObjectType::L2Cache,
-    ObjectType::L3Cache,
-    ObjectType::L4Cache,
-    ObjectType::L5Cache,
-];
 
 /// CPU cache statistics
 ///
@@ -102,6 +84,9 @@ pub struct CpuCacheStats {
 
 impl CpuCacheStats {
     /// Compute CPU cache statistics, if cache sizes are known
+    ///
+    /// Returns None if cache size information is unavailable for at least some
+    /// of the CPU caches on the system.
     pub fn new(topology: &Topology) -> Option<Self> {
         let mut smallest_data_cache_sizes = ArrayVec::new();
         let mut smallest_data_cache_sizes_per_thread = ArrayVec::new();
@@ -138,7 +123,10 @@ impl CpuCacheStats {
                 break;
             }
         }
-        Some(Self {
+
+        // Absence of L1 caches is not possible on current hardware, it
+        // is just interpreted as cache info being unavailable overall
+        (!smallest_data_cache_sizes.is_empty()).then_some(Self {
             smallest_data_cache_sizes,
             smallest_data_cache_sizes_per_thread,
             total_data_cache_sizes,
@@ -151,9 +139,10 @@ impl CpuCacheStats {
     /// hierarchy on this system, and what is the minimal
     /// cache capacity at each level.
     ///
-    /// You should tune sequential algorithms such that their working set is
-    /// smaller than the first reported cache capacity, if not such that it is
-    /// smaller than the second reported cache capacity, and so on.
+    /// You should tune sequential algorithms such that they fit this effective
+    /// cache hierarchy (first layer of loop blocking has a working set that can
+    /// stay in the first reported cache capacity, second layer of loop blocking
+    /// has a working set that can fit in the second reported capacity, etc.)
     pub fn smallest_data_cache_sizes(&self) -> &[u64] {
         &self.smallest_data_cache_sizes[..]
     }
@@ -161,13 +150,15 @@ impl CpuCacheStats {
     /// Smallest CPU data cache capacity at each cache level, per thread
     ///
     /// This tells you how many cache levels there are in the deepest cache
-    /// hierarchy on this system, and what is the minimal
-    /// cache capacity at each level, divided by the number of threads sharing
-    /// that cache.
+    /// hierarchy on this system, and what is the minimal cache capacity _per
+    /// thread sharing a cache_ at each level.
     ///
-    /// In parallel algorithms where the fact that threads share cache cannot be
-    /// leveraged, you should tune the sequential tasks processed by each
-    /// thread such that they fit in the reported cache capacities.
+    /// In parallel algorithms where all CPU threads are potentially used, and
+    /// threads effectively share no common data, you should tune the private
+    /// working set of each thread such that it fits this effective cache
+    /// hierarchy (first layer of loop blocking has a working set that can stay
+    /// in the first reported cache capacity, second layer of loop blocking has
+    /// a working set that can fit in the second reported capacity, etc.).
     pub fn smallest_data_cache_sizes_per_thread(&self) -> &[u64] {
         &self.smallest_data_cache_sizes_per_thread[..]
     }
@@ -194,5 +185,97 @@ impl CpuCacheStats {
     /// parallel algorithm collectively fit into the smallest last-level cache.
     pub fn total_data_cache_sizes(&self) -> &[u64] {
         &self.total_data_cache_sizes[..]
+    }
+}
+
+/// Data (or unified) caches levels supported by hwloc
+const DATA_CACHE_LEVELS: &[ObjectType] = &[
+    ObjectType::L1Cache,
+    ObjectType::L2Cache,
+    ObjectType::L3Cache,
+    ObjectType::L4Cache,
+    ObjectType::L5Cache,
+];
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use similar_asserts::assert_eq;
+
+    #[test]
+    fn stats() {
+        let topology = Topology::test_instance();
+        let stats = topology.cpu_cache_stats();
+
+        if let Some(stats) = stats {
+            // All stats should cover the same number of cache levels
+            let num_cache_levels = stats.smallest_data_cache_sizes().len();
+            assert_eq!(
+                num_cache_levels,
+                stats.smallest_data_cache_sizes_per_thread().len()
+            );
+            assert_eq!(num_cache_levels, stats.total_data_cache_sizes().len());
+
+            // Number of cache levels shouldn't exceed hwloc's ability
+            assert!(num_cache_levels <= DATA_CACHE_LEVELS.len());
+
+            // Now let's look at individual cache levels
+            for (idx, cache_level) in DATA_CACHE_LEVELS.iter().copied().enumerate() {
+                // Should only report nothing for levels with no known cache
+                if idx >= num_cache_levels {
+                    assert_eq!(topology.objects_with_type(cache_level).count(), 0);
+                    continue;
+                }
+
+                // Stats should be accurate
+                let cache_sizes_and_arities = topology.objects_with_type(cache_level).map(|obj| {
+                    let arity = obj.cpuset().unwrap().weight().unwrap();
+                    let Some(ObjectAttributes::Cache(cache)) = obj.attributes() else {
+                        unreachable!()
+                    };
+                    (cache.size().unwrap().get(), arity)
+                });
+                assert_eq!(
+                    stats.smallest_data_cache_sizes()[idx],
+                    cache_sizes_and_arities
+                        .clone()
+                        .map(|(size, _)| size)
+                        .min()
+                        .unwrap()
+                );
+                assert_eq!(
+                    stats.smallest_data_cache_sizes_per_thread()[idx],
+                    cache_sizes_and_arities
+                        .clone()
+                        .map(|(size, arity)| size / arity as u64)
+                        .min()
+                        .unwrap()
+                );
+                assert_eq!(
+                    stats.total_data_cache_sizes()[idx],
+                    cache_sizes_and_arities
+                        .clone()
+                        .map(|(size, _)| size)
+                        .sum::<u64>()
+                );
+            }
+        } else {
+            // Cache stats queries should only fail if hwloc failed to probe CPU
+            // cache properties, overall or cache size specifically.
+            if topology.objects_with_type(ObjectType::L1Cache).count() == 0 {
+                return;
+            }
+            for &cache_level in DATA_CACHE_LEVELS {
+                for obj in topology.objects_with_type(cache_level) {
+                    let Some(ObjectAttributes::Cache(cache)) = obj.attributes() else {
+                        unreachable!()
+                    };
+                    if cache.size().is_none() {
+                        return;
+                    }
+                }
+            }
+            panic!("Cache stats query should not have failed");
+        }
     }
 }
